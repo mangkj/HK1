a6cd5725e894e46a212dd31d89775ee0041b2fb7
Louis <lg2894@nyu.edu>
2018-03-06 15:58:31 -0500

    add explanatory class

---
+++ adjustAndClean/HowToLoop.py
@@ -0,0 +1,47 @@
+from impactModel.FileManager import FileManager
+import numpy as np
+import pandas as pd
+from adjustAndClean.TAQAdjust import TAQAdjust
+from adjustAndClean.TAQCleaner import TAQCleaner
+from adjustAndClean.StackData import StackData
+
+class HowToLoop(object):
+    '''
+    For you Canel and Michael: How to loop & use StackData, TAQAdjust and TAQCleaner
+    '''
+
+    def __init__(self, baseDir, startDate, endDate ):
+        '''
+        '''
+        ticker = 'IBM'
+        
+        # FIRST: Take S&P500 tickers
+        s_p500 = '/media/louis/DATA/documents/cours/NYU/SPRING_18/ATQS/HK1/s_p500.xlsx'
+        s_p500xls = pd.read_excel(open(s_p500,'rb'), sheet_name='WRDS')
+        s_ptickers = np.unique((np.array(s_p500xls['Ticker Symbol'])).astype(str))
+        s_ptickers = s_ptickers[:-1]
+       
+
+        # THEN: Loop through tickers and stack them separately
+        for ticker in s_ptickers:
+            
+            # Stack everything
+            stack = StackData(baseDir, startDate, endDate, ticker)
+            stack.addTrades()
+            stack.addQuotes()
+            
+            # Get results
+            quotes = stack.getStackedQuotes()
+            trades = stack.getStackedTrades()
+            
+            # Adjust
+            adjuster = TAQAdjust( quotes, trades, ticker, s_p500 )
+            adjuster.adjustQuote()
+            adjuster.adjustTrade()
+            
+            # Clean
+            cleaner = TAQCleaner(quotes, trades)
+            cleaner.cleanQuotes()
+            cleaner.cleanTrades()
+            
+        """ The datastructure to store those elements is up to you """
--- adjustAndClean/Test_StackData.py
+++ adjustAndClean/Test_StackData.py
@@ -11,18 +11,8 @@ class Test_StackData(unittest.TestCase):
 
     def test1(self):
         baseDir = '/media/louis/DATA/documents/cours/NYU/SPRING_18/ATQS/HK1/data/sampleTAQ/sampleTAQ'
-        s_p500 = '/media/louis/DATA/documents/cours/NYU/SPRING_18/ATQS/HK1/s_p500.xlsx'
         
         ticker = "IBM"
-        
-        # Test if S&P ticker
-        s_p500xls = pd.read_excel(open(s_p500,'rb'), sheet_name='WRDS')
-        s_ptickers = np.unique((np.array(s_p500xls['Ticker Symbol'])).astype(str))
-        s_ptickers = s_ptickers[:-1]
-        
-        if (not (ticker in s_ptickers)):
-            print("Not a S&P ticker!")
-            return
 
         # Calls to StackData methods
         loopStack = StackData( baseDir, "20070101" , "20070909", ticker )
